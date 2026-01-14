d1502a4cb0bcaed2416b83ac196bed4e519893ae
mingder wang <mingderwang@me.com>
2026-01-14 16:04:19 +0800

    use simpleGrid

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import '@mantine/core/styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MantineProvider, createTheme, type MantineColorsTuple } from '@mantine/core';
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "@mantine/core/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const myColor: MantineColorsTuple = [
+  '#ecf4ff',
+  '#dce4f5',
+  '#b9c7e2',
+  '#94a8d0',
+  '#748dc0',
+  '#5f7cb7',
+  '#5474b4',
+  '#44639f',
+  '#3a5890',
+  '#2c4b80'
+];
+
+const theme = createTheme({
+  colors: {
+    myColor,
+  },
+  primaryColor: 'myColor',
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MantineProvider       theme={{
-        
-        colors: {
-          gray: [
-            "#f8f9fa",
-            "#f1f3f5",
-            "#e9ecef",
-            "#dee2e6",
-            "#ced4da",
-            "#adb5bd",
-            "#868e96",
-            "#495057",
-            "#343a40",
-            "#212529",
-          ],
-        },
-        primaryColor: "gray",
-      }}
-    >
-      <style>
-        {`
-          :root {
-            --mantine-color-body: #ffffff;
-            --mantine-color-text: #525252;
-          }
-        `}
-      </style>
+     <MantineProvider theme={theme}>
         <App />
       </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
